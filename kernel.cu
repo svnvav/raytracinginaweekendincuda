bffe1baa818c5e9450de5ae953686e2ef36043b0
Savinov Aleksandr <a.savinov@plarium.Local>
2019-04-01 16:50:29 +0300

    chapter 7 complete

--- kernel.cu
+++ kernel.cu
@@ -2,9 +2,12 @@
 #include <fstream>
 #include "time.h"
 #include "device_launch_parameters.h"
+#include <curand.h>
+#include <curand_kernel.h>
 
 #include "hitable_list.h"
 #include "sphere.h"
+#include "camera.h"
 
 #define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
 void check_cuda(cudaError_t result, char const *const func, const char *const file, int const line) {
@@ -17,54 +20,94 @@ void check_cuda(cudaError_t result, char const *const func, const char *const fi
 	}
 }
 
-__device__ vec3 color(const ray& r, hitable **world) {
-	hit_record rec;
-	if ((*world)->hit(r, 0.0, FLT_MAX, rec)) {
-		return 0.5f*vec3(rec.normal.x() + 1.0f, rec.normal.y() + 1.0f, rec.normal.z() + 1.0f);
-	}
-	else {
-		vec3 unit_direction = unit_vector(r.direction());
-		float t = 0.5f*(unit_direction.y() + 1.0f);
-		return (1.0f - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
-	}
+#define RANDVEC3 vec3(curand_uniform(local_rand_state),curand_uniform(local_rand_state),curand_uniform(local_rand_state))
+
+__device__ vec3 random_in_unit_sphere(curandState *local_rand_state) {
+	vec3 p;
+	do {
+		p = 2.0f*RANDVEC3 - vec3(1, 1, 1);
+	} while (p.squared_length() >= 1.0f);
+	return p;
 }
 
-__global__ void render(vec3 *fb, int max_x, int max_y,
-	vec3 origin, vec3 lower_left_corner, vec3 horizontal, vec3 vertical,
-	hitable **world) {
+__device__ vec3 color(const ray& r, hitable **world, curandState *local_rand_state) {
+	ray cur_ray = r;
+	float cur_attenuation = 1.0f;
+	for (int i = 0; i < 50; i++) {
+		hit_record rec;
+		if ((*world)->hit(cur_ray, 0.001f, FLT_MAX, rec)) {
+			vec3 target = rec.p + rec.normal + random_in_unit_sphere(local_rand_state);
+			cur_attenuation *= 0.5f;
+			cur_ray = ray(rec.p, target - rec.p);
+		}
+		else {
+			vec3 unit_direction = unit_vector(cur_ray.direction());
+			float t = 0.5f*(unit_direction.y() + 1.0f);
+			vec3 c = (1.0f - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+			return cur_attenuation * c;
+		}
+	}
+	return vec3(0.0, 0.0, 0.0); // exceeded recursion
+}
+
+__global__ void render_init(int max_x, int max_y, curandState *rand_state) {
+	int i = threadIdx.x + blockIdx.x * blockDim.x;
+	int j = threadIdx.y + blockIdx.y * blockDim.y;
+	if ((i >= max_x) || (j >= max_y)) return;
+	int pixel_index = j * max_x + i;
+	//Each thread gets same seed, a different sequence number, no offset
+	curand_init(1984, pixel_index, 0, &rand_state[pixel_index]);
+}
+
+__global__ void render(
+	vec3 *fb,
+	int max_x, int max_y, int ns,
+	camera **cam,
+	hitable **world,
+	curandState *rand_state
+) 
+{
 	int i = threadIdx.x + blockIdx.x * blockDim.x;
 	int j = threadIdx.y + blockIdx.y * blockDim.y;
 	if ((i >= max_x) || (j >= max_y)) return;
 	int pixel_index = j * max_x + i;
 
-	float u = float(i) / float(max_x);
-	float v = float(j) / float(max_y);
-	ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-	fb[pixel_index] = color(r, world);
+	curandState local_rand_state = rand_state[pixel_index];
+	vec3 col(0, 0, 0);
+	for (int s = 0; s < ns; s++) {
+		float u = float(i + curand_uniform(&local_rand_state)) / float(max_x);
+		float v = float(j + curand_uniform(&local_rand_state)) / float(max_y);
+		ray r = (*cam)->get_ray(u, v);
+		col += color(r, world, &local_rand_state);
+	}
+	col /= float(ns);
+	col[0] = sqrt(col[0]);
+	col[1] = sqrt(col[1]);
+	col[2] = sqrt(col[2]);
+	fb[pixel_index] = col;
 }
 
-__global__ void create_world(hitable **d_list, hitable **d_world) {
+__global__ void create_world(hitable **d_list, hitable **d_world, camera **d_camera) {
 	if (threadIdx.x == 0 && blockIdx.x == 0) {
 		*(d_list) = new sphere(vec3(0, 0, -1), 0.5);
 		*(d_list + 1) = new sphere(vec3(0, -100.5, -1), 100);
 		*d_world = new hitable_list(d_list, 2);
+		*d_camera = new camera();
 	}
 }
 
-__global__ void free_world(hitable **d_list, hitable **d_world) {
+__global__ void free_world(hitable **d_list, hitable **d_world, camera **d_camera) {
 	delete *(d_list);
 	delete *(d_list + 1);
 	delete *d_world;
+	delete *d_camera;
 }
 
 int main()
 {
-	int nx = 1024;
-	int ny = 512;
-	vec3 origin(0 ,0, 0);
-	vec3 lower_left_corner(-2, -1, -1);
-	vec3 horizontal(4, 0, 0);
-	vec3 vertical(0, 2, 0);
+	int nx = 576;
+	int ny = 288;
+	int ns = 50;
 
 	int tx = 8;
 	int ty = 8;
@@ -78,12 +121,15 @@ int main()
 	start = clock();
 
 	checkCudaErrors(cudaMallocManaged((void **)&fb, fb_size));
-	// make our world of hitables
+
+		// make our world of hitables & the camera
 	hitable **d_list;
 	checkCudaErrors(cudaMalloc((void **)&d_list, 2 * sizeof(hitable *)));
 	hitable **d_world;
 	checkCudaErrors(cudaMalloc((void **)&d_world, sizeof(hitable *)));
-	create_world << <1, 1 >> > (d_list, d_world);
+	camera **d_camera;
+	checkCudaErrors(cudaMalloc((void **)&d_camera, sizeof(camera *)));
+	create_world <<<1, 1>>> (d_list, d_world, d_camera);
 	checkCudaErrors(cudaGetLastError());
 	checkCudaErrors(cudaDeviceSynchronize());
 
@@ -93,9 +139,16 @@ int main()
 
 	start = clock();
 
-	dim3 blocks(nx / tx + 1, ny / ty + 1);
+	dim3 blocks((nx - 1) / tx + 1, (ny - 1) / ty + 1);
 	dim3 threads(tx, ty);
-	render <<<blocks, threads>>> (fb, nx, ny, origin, lower_left_corner, horizontal, vertical, d_world);
+
+	curandState *d_rand_state;
+	checkCudaErrors(cudaMalloc((void **)&d_rand_state, num_pixels * sizeof(curandState)));
+	render_init << <blocks, threads >> > (nx, ny, d_rand_state);
+	checkCudaErrors(cudaGetLastError());
+	checkCudaErrors(cudaDeviceSynchronize());
+
+	render <<<blocks, threads>>> (fb, nx, ny, ns, d_camera, d_world, d_rand_state);
 	checkCudaErrors(cudaGetLastError());
 	checkCudaErrors(cudaDeviceSynchronize());
 
@@ -125,10 +178,11 @@ int main()
 
 	// clean up
 	checkCudaErrors(cudaDeviceSynchronize());
-	free_world << <1, 1 >> > (d_list, d_world);
+	free_world <<<1, 1>>> (d_list, d_world, d_camera);
 	checkCudaErrors(cudaGetLastError());
-	checkCudaErrors(cudaFree(d_list));
+	checkCudaErrors(cudaFree(d_camera));
 	checkCudaErrors(cudaFree(d_world));
+	checkCudaErrors(cudaFree(d_list));
 	checkCudaErrors(cudaFree(fb));
 
 	// useful for cuda-memcheck --leak-check full
